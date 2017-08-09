695ec9bc54961b5a3de4aad5c50721fbbe5665de
soumitra mandal <soumitra@iandwe.in>
2017-08-10 02:19:07 +0530

    going to release 10th August 2017

--- contacts.php
+++ contacts.php
@@ -114,37 +114,41 @@ else{
 <![endif]-->    
 </head>
 <body>
-	<!--header-->
+	
+    <!--fb code -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.10&appId=416279145395960";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+    <!--etodur e -->
+
+    <!--header-->
     <div class="header">
-    	<div class="wrap">
-        	<div class="navbar navbar_ clearfix">
-            	<div class="container">
+        <div class="wrap">
+            <div class="navbar navbar_ clearfix">
+                <div class="container">
                     <div class="row">
                         <div class="span4">
-                        	<div class="logo"><a href="index.html"><img src="img/logo.jpg" alt="" /></a></div>                        
+                            <div class="logo"><a href="index.html"><img src="img/logo.jpg" alt="" /></a></div>                        
                         </div>
                         <div class="span8">
-                        	<div class="follow_us">
-                                <ul>
-                                    <li><a href="#" class="facebook">Facebook</a></li>
-                                    <li><a href="#" class="vimeo">Vimeo</a></li>
-                                    <li><a href="#" class="tumbrl">Tumbrl</a></li>
-                                    <li><a href="#" class="twitter">Twitter</a></li>
-                                    <li><a href="#" class="delicious">Delicious</a></li>
-                                </ul>
-                            </div>
+                            
                             <div class="clear"></div>
                             <nav id="main_menu">
                                 <div class="menu_wrap">
                                     <ul class="nav sf-menu">
                                       <li><a href="index.html">Home</a></li>
-                                      <li><a href="about.html">About</a></li>
+                                      <li class="current"><a href="about.html">About</a></li>
                                       
                                       <li class="sub-menu"><a href="portfolio_3columns.html">Designs</a>
                                            
                                       </li>                                  
-                                      
-                                      <li class="current"><a href="contacts.html">Contacts</a></li>
+                                     
+                                      <li><a href="contacts.php">Contacts</a></li>
                                     </ul>
                                 </div>
                              </nav>                            
@@ -154,7 +158,8 @@ else{
              </div>
         </div>    
     </div>
-    <!--//header-->
+    
+    <!--//header-->    
         
     <!--page_container-->
     <div class="page_container">
@@ -201,105 +206,88 @@ else{
     <!--//page_container-->
     
     <!--footer-->
-    <div id="footer">
-    	<div class="wrap">
-    		<div class="container">
-            	<div class="row">
-                	<div class="span3">
-                    	                     
-                    </div>
-					
-                    <div class="span3">
-                    	
-                        <!-- <form action="#" method="post">
-                        	<input class="span3" type="text" name="inputName" id="inputName" value="Name" onFocus="if (this.value == 'Name') this.value = '';" onBlur="if (this.value == '') this.value = 'Name';" />
-                            <input class="span3" type="text" name="inputEmail" id="inputEmail" value="Email" onFocus="if (this.value == 'Email') this.value = '';" onBlur="if (this.value == '') this.value = 'Email';" />
-                            <textarea name="inputContent" id="inputContent" class="span3" onFocus="if (this.value == 'Message') this.value = '';" onBlur="if (this.value == '') this.value = 'Message';" >Message</textarea>
-                            <div class="clear"></div>
-                            <input type="reset" class="btn dark_btn" value="Clear form" />
-                            <input type="submit" class="btn send_btn" value="Send!" />
-                            <div class="clear"></div>
-                        </form>
-                    </div> 
-                    <div class="span3">
-                    	<h2 class="title">Testimonials</h2>
-                        <ul>
-                        	<li>
-                            	<span class="testimonials_arrow"></span>Write your testimonials here...
-                            	<div class="clear"></div>
-                                <div class="author">Innovative Traders, Company inc.</div>
-                            </li>
-                            <li>
-                            	<span class="testimonials_arrow"></span>Write your testimonials here...
-                            	<div class="clear"></div>
-                                <div class="author">F Ahmed</div>
-                            </li>
-                        </ul>                     
-                    </div> -->
-                    <div class="span3">
-                    	
-                        </div> 
-                    </div>         	
-            	</div>
-        	</div>            
-        </div>
-        
-        <div class="footer_bottom">
+<div class="footer_bottom">
             <div class="wrap">
-            	<div class="container">
-                	<div class="row">
-                		<div class="span5">
-                        	<div class="foot_logo"><a href="index.html"><img src="img/foot_logo.jpg" alt="" /></a></div>    
-                        	<div class="copyright">&copy; 2020 Innovative Traders. All Rights Reserved.</div>                        
+                <div class="container">
+                    <div class="row">
+                        <div class="span5">
+                            <div class="foot_logo"><a href="index.html"><img src="img/foot_logo.jpg" alt="" /></a></div>    
+                            <div class="copyright">&copy; 2020 Innovative Traders. All Rights Reserved.</div>                        
                         </div>
+                        
                         <div class="span7">
-                        	<div class="foot_right_block">
-                            	<div class="fright">
-                                	<form action="#" method="post">
+                            <!--deleted 
+                            <div class="foot_right_block">
+                                <div class="fright">
+                                    <form action="#" method="post">
                                         <input class="inp_search" name="name" type="text" value="Search the Site" onfocus="if (this.value == 'Search the Site') this.value = '';" onblur="if (this.value == '') this.value = 'Search the Site';" />                                        
-                                    </form>
-                                </div>
+                                        </form>
+                                </div>  -->
+                                
+                <div class="foot_fb">
+        
+            
+            <div class="fb-like" data-href="https://www.facebook.com/innovativetraders563" data-width="200" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+        
+                                <!-- 
                                 <div class="follow_us">
-                                	<ul>
-                                        <li><a rel="tooltip" href="#" title="Facebook" class="facebook">Facebook</a></li>
+                                    <ul>
+                                        <li><a rel="tooltip" href="https://www.facebook.com/innovativetraders563/" title="Facebook" class="facebook">Facebook</a></li>
                                         <li><a rel="tooltip" href="#" title="Twitter" class="twitter">Twitter</a></li>
                                         <li><a rel="tooltip" href="#" title="Tumbrl" class="tumbrl">Tumbrl</a></li>
                                         <li><a rel="tooltip" href="#" title="Vimeo" class="vimeo">Vimeo</a></li>
-                                        <li><a rel="tooltip" href="#" title="Delicious" class="delicious">Delicious</a></li>
+                                        <li><a rel="tooltip" href="#" title="Delicious" class="delicious">Delicious</a>
                                     </ul>
                                 </div>
+                                </li> -->
                                 <div class="clear"></div>
                             
-                            	<div class="clear"></div>
-                            	<div class="foot_menu">
-                            		<ul>
-                                        <li><a href="index.html">Home</a></li>
+                                <div class="clear"></div>
+                                <div class="foot_menu">
+                                    <ul>
+                                        <li><a href="index.html" class="current">Home</a></li>
                                         <li><a href="about.html">About</a></li>
-                                      
                                         <li><a href="portfolio_3columns.html">Designs</a></li>
-                                       
-                                        <li><a href="contacts.html" class="current">Contacts</a></li>
+                                        <li><a href="contacts.php">Contacts</a></li>
                                     </ul>
-                            	</div>
+                                </div>
                             </div>                            
                         </div>
-                    </div>	
+                    </div>  
                 </div>
             </div>
         </div>
     </div>
     <!--//footer-->    
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script src="js/google-code-prettify/prettify.js"></script>
+    <!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-598b609fb57641e4"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
     <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="js/jquery.mobile.customized.min.js"></script>
+    <script type="text/javascript" src="js/camera.js"></script>
     <script src="js/bootstrap.js"></script>
     <script src="js/superfish.js"></script>
-    <script type="text/javascript" src="js/jquery.tweet.js"></script>
     <script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+    <script type="text/javascript" src="js/jquery.jcarousel.js"></script>
+    <script type="text/javascript" src="js/jquery.tweet.js"></script>
     <script type="text/javascript" src="js/myscript.js"></script>
-    <script src="js/application.js"></script>
-    
+    <script type="text/javascript">
+        $(document).ready(function(){   
+            //Slider
+            $('#camera_wrap_1').camera();
+            
+            //Featured works & latest posts
+            $('#mycarousel, #mycarousel2, #newscarousel').jcarousel();                                                  
+        });     
+    </script>
+    <!-- Load Facebook SDK for JavaScript -->
+  <div id="fb-root"></div>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>    
 </body>
 </html>
 <?php
